03739789cb61719d607e1849a081c9c090df71c3
Dani Blackwell <bleuxgenius@gmail.com>
2020-01-17 15:34:53 -0600

    MVP I think
    Im not sure why i can not pull the data, from the array but i think i am completed.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { findByText, render} from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('is it rendering ', () => {
+  render(<App/>)
+}) 
+
+test("search is found in search bar", () => {
+  const { getByText } = render(<App />);
+  getByText(/search/i);
 });
